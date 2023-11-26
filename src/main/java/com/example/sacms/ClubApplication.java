6a059b9a12209f60915956e1e9ea26f6a7a8ebe1
Praveen Peiris <129660222+peirispraveen@users.noreply.github.com>
2023-11-26 21:16:56 +0530

    Test

--- src/main/java/com/example/sacms/ClubApplication.java
+++ src/main/java/com/example/sacms/ClubApplication.java
@@ -11,7 +11,6 @@ import java.sql.SQLException;
 
 public class ClubApplication extends Application {
 
-
     @Override
     public void start(Stage stage) throws IOException, SQLException {
         Storage.allAvailables();
