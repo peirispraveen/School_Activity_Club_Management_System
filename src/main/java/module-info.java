0293bce45a54c06d03d161210740e6ab568de8c5
Sankalpa Kulathunga <sankalpakulathunga123@gmail.com>
2023-11-26 21:07:06 +0530

    add attendance tracking

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,14 @@
 module com.example.sacms {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
     requires java.sql;
     requires mysql.connector.j;
     requires org.apache.poi.poi;
     requires org.apache.poi.ooxml;
 
-
     opens com.example.sacms to javafx.fxml;
+    opens com.example.demo to javafx.fxml;
     exports com.example.sacms;
+    exports com.example.demo;
 }
