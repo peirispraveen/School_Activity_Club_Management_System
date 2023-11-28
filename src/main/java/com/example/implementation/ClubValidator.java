fdc9b12dcf40528a169ae81d477b3e6c01eae45a
《《 THE BOSS 》》 <muaadhnasly2005@gmail.com>
2023-11-28 17:17:00 +0530

    added java test

--- src/main/java/com/example/implementation/ClubValidator.java
+++ src/main/java/com/example/implementation/ClubValidator.java
@@ -11,30 +11,36 @@ public class ClubValidator implements ClubValidation {
     ClubAdvisor advisor;
     Date date;
 
+    @Override
     public boolean validateId(String id) {
         String regex="^[^;]{1,5}$";
         return id.matches(regex);
 
     }
+    @Override
     public boolean validateName(String name){
         String regex="^[^;]{1,20}$";
         return name.matches(regex);
     }
 
+    @Override
     public boolean validateDescription(String description){
         String regex="^[^;]{1,60}$";
         return description.matches(regex);
     }
 
+    @Override
     public boolean validateAdvisor(String advisor){
         String regex="^[^;]{1,20}$";
         return advisor.matches(regex);
     }
 
+    @Override
     public boolean validateMaxParticipants(String maxParticipants){
         String regex = "^[1-9]\\d{0,2}$";
         return maxParticipants.matches(regex);
     }
+    @Override
     public boolean validateDate(String date) {
         String regex = "^\\d{4}-\\d{2}-\\d{2}$";
 
