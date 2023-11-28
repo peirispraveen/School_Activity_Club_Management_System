52bb8202a20864ce8cdd7acf3e2ae7a34b84a84f
《《 THE BOSS 》》 <muaadhnasly2005@gmail.com>
2023-11-28 17:15:18 +0530

    added java test

---
+++ src/main/java/com/example/implementation/ClubValidator.java
@@ -0,0 +1,53 @@
+package com.example.implementation;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+public class ClubValidator implements ClubValidation {
+    String id;
+    String name;
+    String description;
+    ClubAdvisor advisor;
+    Date date;
+
+    public boolean validateId(String id) {
+        String regex="^[^;]{1,5}$";
+        return id.matches(regex);
+
+    }
+    public boolean validateName(String name){
+        String regex="^[^;]{1,20}$";
+        return name.matches(regex);
+    }
+
+    public boolean validateDescription(String description){
+        String regex="^[^;]{1,60}$";
+        return description.matches(regex);
+    }
+
+    public boolean validateAdvisor(String advisor){
+        String regex="^[^;]{1,20}$";
+        return advisor.matches(regex);
+    }
+
+    public boolean validateMaxParticipants(String maxParticipants){
+        String regex = "^[1-9]\\d{0,2}$";
+        return maxParticipants.matches(regex);
+    }
+    public boolean validateDate(String date) {
+        String regex = "^\\d{4}-\\d{2}-\\d{2}$";
+
+        if (!date.matches(regex) ) {
+            return false;
+        }
+        try {
+            LocalDate givenDate = LocalDate.parse(date);
+            LocalDate currentDate = LocalDate.now();
+            return !givenDate.isAfter(currentDate);
+        }catch (DateTimeParseException ignored){
+            return false;
+        }
+    }
+
+}
